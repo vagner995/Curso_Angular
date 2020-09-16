0a855b5054fea19afe06b9185a4c3d7a62d0049e
Vágner <vagner995@hotmail.com>
2020-09-16 18:49:56 -0300

    Primeira adição do projeto. O exemplo da aula até agora é só um component na pasta app.

---
+++ my-app/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+     selector: 'meu-primeiro-component',
+     template: `
+      <p>Component de Angular 2!</p>
+     `
+})
+export class MeuPrimeiroComponent {}
